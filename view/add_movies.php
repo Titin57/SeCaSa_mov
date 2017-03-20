68db7e14c4f38f8ce7eb7369a7836f44f3d71a6d
samwitry <54mw1tr7@gmail.com>
2017-03-20 16:45:54 +0100

    i changed the variables... we can test the file

--- view/add_movies.php
+++ view/add_movies.php
@@ -1,13 +1,6 @@
 <div class="panel panel-primary">
   <div class="panel-heading">
-    <h3 class="panel-title">
-
-    <?php /* if (!empty($studentInfos['stu_id'])) : ?>
-    	Modification de <?= $studentInfos['stu_firstname'] ?> <?= $studentInfos['stu_lastname'] ?>
-    <?php else : ?>
-    	Ajout d'un étudiant
-    <?php endif; */?>
-    </h3>
+    <h3 class="panel-title">Add a film</h3>
   </div>
   <div class="panel-body">
   	<?php /*if (!empty($errorList)) : ?>
@@ -18,64 +11,55 @@
 		</div>
   	<?php endif;*/ ?>
 	<form action="" method="post" enctype="multipart/form-data">
-		<?php /*if (!empty($studentInfos['stu_image'])) :?>
-			<img src="<?= $studentInfos['stu_image'] ?>" alt="" height="140" style="display:block;margin:auto;" /><br>
-		<?php endif; */?>
+		<?php if (!empty($filmInfos['mov_post'])) :?>
+			<img src="<?= $filmInfos['mov_post'] ?>" alt="" height="140" style="display:block;margin:auto;" /><br>
+		<?php endif; ?>
   		<div class="row">
 	  		<div class="col-md-6 col-sm-6 col-xs-12">
 
 				<div class="form-group">
-					<label>Nom</label>
-					<input type="text" class="form-control" name="stu_lastname" value="<?=/* $studentInfos['stu_lastname'] */?>" placeholder="Nom" />
+					<label>movie title</label>
+					<input type="text" class="form-control" name="mov_title" value="<?= $filmInfos['mov_title'] */?>" placeholder="movie title" />
 				</div>
 
 				<div class="form-group">
-					<label>Prénom</label>
-					<input type="text" class="form-control" name="stu_firstname" value="<?=/* $studentInfos['stu_firstname'] */?>" placeholder="Prénom" />
+					<label>actors</label>
+					<input type="text" class="form-control" name="mov_actors" value="<?= $filmInfos['mov_actors'] */?>" placeholder="actors" />
 				</div>
 
 				<div class="form-group">
-					<label>Email</label>
-					<input type="text" class="form-control" name="stu_email" value="<?=/* $studentInfos['stu_email'] */?>" placeholder="Email" />
+					<label>file name</label>
+					<input type="text" class="form-control" name="mov_fileName" value="<?= $filmInfos['mov_fileName'] ?>" placeholder="file name" />
 				</div>
 
 				<div class="form-group">
-					<label>Date de naissance</label>
-  					<input type="text" class="form-control" name="stu_birthdate" value="<?=/* $studentInfos['stu_birthdate'] */?>" placeholder="Date de naissance" />
-  					<small class="form-text text-muted">au format YYYY-MM-DD (<?= date('Y-m-d') ?>)</small>
+					<label>release date</label>
+  					<input type="text" class="form-control" name="mov_rel" value="<?=$filmInfos['mov_rel'] ?>" placeholder="release date" />
+  					<small class="form-text text-muted">format in YYYY-MM-DD (<?= date('Y-m-d') ?>)</small>
   				</div>
 			</div>
 	  		<div class="col-md-6 col-sm-6 col-xs-12">
 
 				<div class="form-group">
-					<label>Sympathie</label>
+					<label>genre</label>
+                    <input type="text" class="form-control" name="gen_name" value="<?=$filmInfos['gen_name'] ?>" placeholder="genre" />
+
+                    <!--
 					<select name="stu_friendliness" class="form-control">
 						<option value="">choisissez</option>
 						<?php/*for($i=0;$i<=5;$i++) : ?>
 							<option value="<?= $i ?>"<?= $studentInfos['stu_friendliness'] == $i ? ' selected=selected"' : '' ?>><?= getSympathieName($i) ?></option>
 						<?php endfor; */?>
 					</select>
+                    -->
 				</div>
 
 				<div class="form-group">
-					<label>Session</label>
-					<select name="ses_id" class="form-control">
-						<option value="">choisissez</option>
-						<?php /*foreach ($sessionsList as $sesId=>$sesName) : ?>
-							<option value="<?= $sesId ?>"<?= $studentInfos['session_ses_id'] == $sesId ? ' selected=selected"' : '' ?>><?= $sesName ?></option>
-						<?php endforeach;*/ ?>
-					</select>
+					<label>film plot</label>
+					<textarea rows="4" cols="50"  class="form-control" name="mov_plot" value="<?=$filmInfos['mov_plot'] ?>" placeholder="film plot" >
+                    </textarea>
 				</div>
 
-				<div class="form-group">
-					<label>Ville</label>
-					<select name="cit_id" class="form-control">
-						<option value="">choisissez</option>
-						<?php foreach ($citiesList as $citId=>$citName) : ?>
-							<option value="<?= /*$citId ?>"<?= $studentInfos['city_cit_id'] == $citId ? ' selected=selected"' : '' ?>><?= $citName ?></option>
-						<?php endforeach; */?>
-					</select>
-				</div>
 
 				<div class="form-group">
 					<label>Image</label>
@@ -83,12 +67,12 @@
 				</div>
 			</div>
 		</div>
-		<?php /* here is the link
+		<?php
 
-        if (!empty($studentInfos['stu_id'])) : ?>
-			<input type="submit" class="btn btn-success btn-block" value="Modifier" />
+        if (!empty($filmInfos['film_id'])) : ?>
+			<input type="submit" class="btn btn-success btn-block" value="Modify" />
     	<?php else : ?>
-			<input type="submit" class="btn btn-success btn-block" value="Ajouter" />
+			<input type="submit" class="btn btn-success btn-block" value="Ajdd" />
 		<?php endif; */?>
 	</form>
   </div>
