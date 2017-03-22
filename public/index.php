0f0317939fbeb2ba5d8f8058408fe1923e3f076c
CarloSpecchio <flaviodemello@gmail.com>
2017-03-22 15:32:00 +0100

    inserted pictures with link to movies inserted genres with linkt to genres

--- public/index.php
+++ public/index.php
@@ -4,46 +4,55 @@
 // double dirname to go to parent directory
 require dirname(dirname(__FILE__)).'/inc/config.php';
 
-/* // Code for this page here
-$currentPage = '';
+// Stats nb genres
+/* Code for link max movies by genre
+		4 results should be shown 
+		get all genre by name  */
 
-// Get all movies
-$moviesList = array();
-$sql = '
-	SELECT * 
-	FROM movies
-	
-';
-$sth = $pdo->query($sql);
-if ($sth === false) {
-	print_r($sth->errorInfo());
-}
-else {
-	while ($row = $sth->fetchAll(PDO::FETCH_ASSOC)) {
-		$title = $row['tra_name'].' à '.$row['loc_name'];
-		$moviesList[$title][] = $row;
+	$genreList = array();
+	$sql = '
+		SELECT COUNT(*) AS nb, gen_name
+		FROM genres
+		INNER JOIN movies ON genres.gen_id = movies.genres_gen_id
+		GROUP BY gen_name
+		ORDER BY nb DESC, gen_name ASC
+		LIMIT 4
+	';
+	$sth = $pdo->query($sql);
+	if ($sth === false) {
+		print_r($pdo->errorInfo());
 	}
-}
-/*
-// Stats nb students par ville
-$sql = '
-	SELECT count(*) as nb, cit_name
-	FROM city
-	INNER JOIN student ON student.city_cit_id = city.cit_id
-	GROUP BY cit_name
-	ORDER BY nb DESC, cit_name ASC
-';
-$sth = $pdo->query($sql);
-if ($sth === false) {
-	print_r($pdo->errorInfo());
-}
-else {
-	$studentsVilleList = $sth->fetchAll(PDO::FETCH_ASSOC);
-}*/
+	else {
+		$genreList = $sth->fetchAll(PDO::FETCH_ASSOC);
+		
+	}
 
+/* Stats nb movies (limit to 4 movies)
+ 	Code for movies here
+	results should be shown get all movies by name 
+	Limit to 4 movies */
+
+	$moviesList = array();
+	$sql = '
+		SELECT mov_title, mov_post
+		FROM movies
+		GROUP BY mov_title
+		ORDER BY mov_title ASC
+		LIMIT 4
+	';
+	$sth = $pdo->query($sql);
+	if ($sth === false) {
+		print_r($pdo->errorInfo());
+	}
+	else {
+		$moviesList = $sth->fetchAll(PDO::FETCH_ASSOC);
+		
+	}
+
+		
 // FIN DE MON CODE POUR CETTE PAGE
 
 // At the end, always all view pages */
 include dirname(dirname(__FILE__)).'/view/header.php';
-// include dirname(dirname(__FILE__)).'/view/home.php';
+include dirname(dirname(__FILE__)).'/view/home.php';
 include dirname(dirname(__FILE__)).'/view/footer.php';
--- view/home.php
+++ view/home.php
@@ -1,50 +1,54 @@
-<?php #foreach ($sessionsList as $currentLocation=>$currentSessionsList) :?>
+<!--- The home.php page shows following items:
+	a) inlcude header.php with 3 links (Home, Genres, Add a Movie) and a search field to search the local DB
+	b) a description field for the project
+--> 
 
-	<div class="panel panel-primary">
-	  <div class="panel-heading">
-	    <h3 class="panel-title"><?= $currentLocation ?></h3>
-	  </div>
-		<!-- Table -->
-		<table class="table">
-		<thead>
-			<tr>
-				<th></th>
-				<th>Début</th>
-				<th>Fin</th>
-			</tr>
-		</thead>
-		<tbody>
-		<?php /*foreach ($currentSessionsList as $currentRow) : ?>
-			<tr>
-				<td><a href="list.php?sesId=<?= $currentRow['ses_id'] ?>">#<?= $currentRow['ses_number'] ?></a></td>
-				<td><a href="list.php?sesId=<?= $currentRow['ses_id'] ?>"><?= $currentRow['ses_start_date'] ?></a></td>
-				<td><a href="list.php?sesId=<?= $currentRow['ses_id'] ?>"><?= $currentRow['ses_end_date'] ?></a></td>
-			</tr>
-		<?php endforeach; */?>
-		</tbody>
-		</table>
+<div class="jumbotron">
+	<h2>Project: Movie database.</h2>
+	<div class="container">
+		<p>The database contains a movie collection.
+		In this project, the database can be populated with all data related to the movie like ...</p>
+		<p>... title, release date, movie descritpion, actor(s), genre(s), on which support the movie can be found ...</p>
+		<p>... and much more ...</p>
 	</div>
-
-<?php #endforeach; ?>
-
-<div class="panel panel-primary">
-	<div class="panel-heading">
-		<h3 class="panel-title">Statistiques sur le nombre d'étudiants par ville</h3>
-	</div>
-	<table class="table">
-	<thead>
-		<tr>
-			<th>Ville</th>
-			<th>Nombre d'étudiants</th>
-		</tr>
-	</thead>
-	<tbody>
-		<?php /*foreach ($studentsVilleList as $row) : ?>
-		<tr>
-			<td><?= $row['cit_name'] ?></td>
-			<td><?= $row['nb'] ?></td>
-		</tr>
-		<?php endforeach; */?>
-	</tbody>
-	</table>
 </div>
+
+<!--- Search field start here -->
+
+<form action="list.php" class="navbar-form navbar-center " method="get">
+	<div class="form-group">
+		<input type="text" name="s" class="form-control input-lg" placeholder="Search">
+	</div>
+	<button type="submit" class="btn btn-success btn-lg">OK</button>
+</form>
+
+<!--- Search stops here -->
+
+<div>
+	
+	<?php foreach ($genreList as $row) : ?>
+	<tr>
+		<!--- href to the genre summary page -->
+		<a href = ''><td><?= $row['gen_name'] ?></td><a/>
+		<td><?= $row['nb'] ?></td>
+	</tr>
+	<?php endforeach; ?>
+</div>
+<!--- end of max movies by genre code here -->
+
+<!--- Code for thumbnails of 4 movies here with title- displayed randomly ... --> 
+<div>
+	<h3>Movies </h3>
+</div>
+<div>
+	
+
+	<?php foreach ($moviesList as $row) : ?>
+		<!--- href to the movie detail page -->
+		<a href = ''><img src='<?= $row['mov_post'] ?>' style ='width: 100px; height: 100px' ></img></a>
+		<td><?= $row['mov_title'] ?></td>
+	<?php endforeach; ?>
+
+</div>
+
+<!--- end of thumbnail code here -->
