23920ec54a466ffff39106031ae623616c7a0564
Seb <m_donovan@hotmail.fr>
2017-03-24 09:37:55 +0100

    catalogue des films

--- view/list.php
+++ view/list.php
@@ -1,6 +1,6 @@
 <div class="panel panel-primary">
   <!-- Default panel contents -->
-  <div class="panel-heading">Liste des étudiants</div>
+  <div class="panel-heading">Liste des films</div>
   <div class="panel-body">
   	<?php /*if (!empty($_GET['deleted'])) : ?>
 		<div class="alert alert-success" role="alert">
@@ -20,13 +20,58 @@
   <!-- Table -->
   <table class="table">
   <thead>
-  	<tr>
-		<th>ID</th>
-		<th>Nom</th>
-		<th>Prénom</th>
-		<th>Email</th>
-		<th>Date de naissance</th>
-		<th></th>
+                <tr>
+                	<th>Affiche</th>
+                    <th>Titre du film</th>
+                    <th>Résumé</th>
+                    <th>trucmuch</th>
+
+                </tr>
+  </thead>
+  <thead>
+  	<tr><?php
+
+  		foreach ($movieList as $Index => $film) {
+			//print_r($Index);
+			//print_r($film['mov_title']);
+		}
+  		for ($i=0; $i<=$Index; $i++) 
+							{ 
+						?>
+							<tr>
+						<?php		// pour chaque colonne (de la ligne)
+								for ($j=1; $j<=1; $j++) 
+								{ 
+						?>		
+						<td>
+							<!-- -------------------------
+							// DONNEES A AFFICHER dans la cellule -->
+							<img src="<?php echo $movieList[$i]['mov_post']; ?>">
+						</td>
+						<td>
+						<?php			// -------------------------
+									// DONNEES A AFFICHER dans la cellule
+									echo $movieList[$i]['mov_title']; // CONTENU de la CELLULE (exemple)
+									// -------------------------
+						?>		</td>
+						<td>
+						<?php			// -------------------------
+									// DONNEES A AFFICHER dans la cellule
+									echo $movieList[$i]['mov_plot']; // CONTENU de la CELLULE (exemple)
+									// -------------------------
+						?>		</td>
+						
+						<?php	} // end for
+						?>
+							</tr>
+						<?php
+							} // end for
+						?>
+		<!-- <th>Affiche</th>
+		<th>Titre du film</th>
+		<th>Synopsis</th>
+		<th>Détail</th>
+		<th>modifier</th> -->
   	</tr>
   </thead>
   <tbody>
